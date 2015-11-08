2e9a21669fabcd3f991f7d218d74475bed3a0501
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-11-08 01:24:01 -0500

    added validation for fields

--- odot/spec/features/todo_lists/create_spec.rb
+++ odot/spec/features/todo_lists/create_spec.rb
@@ -49,21 +49,39 @@ describe "Creating todo lists" do
 	end
 
 
-		it "displays an error when the todo list has a description less than 3 characters" do
+		it "displays an error when the todo list has no description" do
 		expect(TodoList.count).to eq(0)
 
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New Todo List")
 
-		fill_in "Title", with: "Hi"
-		fill_in "Description", with: "This is what I'm doing today."
+		fill_in "Title", with: "My List"
+		fill_in "Description", with: "Hi"
 		click_button "Create Todo list"
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
 
 		visit "/todo_lists"
-		expect(page).to_not have_content("This is what I'm doing today.")
+		expect(page).to_not have_content("Hi")
+	end
+
+		it "displays an error when the todo list has a description less than 5 characters" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New Todo List")
+
+		fill_in "Title", with: "My List"
+		fill_in "Description", with: "Hi"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Hi")
 	end
 end
